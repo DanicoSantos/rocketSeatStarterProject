eb9ba1dacf7a3812fd7e6ec1ea9693eeb60d3c3b
Daniel Santos <dados.dan.santos@gmail.com>
2020-10-24 16:27:13 -0300

    feat: add babel dependencies to project

---
+++ bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _require = require("lodash"),
+    methodOf = _require.methodOf;
+
+var Teste = function Teste() {
+  _classCallCheck(this, Teste);
+};
---
+++ main.js
@@ -0,0 +1,5 @@
+const { methodOf } = require("lodash")
+
+class Teste {
+  
+}
